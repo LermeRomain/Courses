3f229a9743ac5f2ecc0e61b9ed49e0e14b1be5d2
romai <romainlerme@hotmail.fr>
2022-02-23 10:42:08 +0100

    end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    const [items, setItems] = useState([]);
+
+    const [inputValue, setInputValue] = useState('');
+
+    const Ajouter = () => {
+        const newItem = {
+            itemName: inputValue,
+            isSelected: false,
+        };
+
+        const newItems = [ ...items, newItem];
+
+        setItems(newItems);
+        setInputValue('');
+    };
+
+
+    const Supprimer = (index) => {
+        const newItems = [ ...items];
+
+        newItems[index].isSelected = !newItems[index].isSelected;
+
+        setItems(newItems);
+    };
+
+
+    return (
+        <div className='app-background'>
+            <div className='main-container'>
+                <h1>Liste des courses</h1>
+                <div className='list'>
+                    {items.map((item, index) => (
+                        <div className='container'>
+                            <div className='name' onClick={() => Supprimer(index)}>
+                                {item.isSelected ? (
+                                    <>
+                                        <span className='supprimer'>{item.itemName}</span>
+                                    </>
+                                ) : (
+                                    <>
+                                        <span>{item.itemName}</span>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <h2>Ajouter un nouvel item à la liste :</h2>
+                <div className='box'>
+                    <input value={inputValue} onChange={(event) => setInputValue(event.target.value)}
+                           className='input-item' placeholder='Ajouter un produits'/>
+                </div>
+                    <button onClick={() => Ajouter()}>Ajouter</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
